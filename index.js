3be94502dcba8315fe60e0df6181ee0f13ae2e7d
TheDarkSid3r <sid3rdark@gmail.com>
2021-11-07 22:33:34 -0800

    whoops don't need that

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ window.addEventListener("load", () => {
     };
 
     const random = (t, e) => Math.random() * (e - t) + t;
-    const randomInt = (t, e) => Math.floor(Math.random() * (e - t)) + t;
 
     const LETTERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     LETTERS.forEach(l => {
